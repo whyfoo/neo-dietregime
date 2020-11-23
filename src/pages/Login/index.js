9f0a27a50336bb4f82d902f78b9ccf1724ca2106
Haidar Hanif <45656365+whyfoo@users.noreply.github.com>
2020-11-23 10:39:29 +0700

    logscreen

---
+++ src/pages/Login/index.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  TouchableHighlight,
+} from 'react-native';
+
+const Login = () => {
+  const win = Dimensions.get('window');
+  return (
+    <View style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <Image
+          source={{
+            uri:
+              'https://images.unsplash.com/photo-1594890716890-16b1dde476ce?ixlib=rb-1.2.1&auto=format&fit=crop&w=924&q=80',
+            width: win.width,
+            height: win.height / 2.5,
+          }}
+          style={{}}
+        />
+      </View>
+
+      <View
+        style={{
+          backgroundColor: 'white',
+          flex: 1,
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+        <Text
+          style={{
+            fontSize: 23,
+            margin: 30,
+            paddingVertical: 25,
+            justifyContent: 'center',
+            textAlign: 'center',
+            fontFamily: 'Quicksand',
+          }}>
+          Personalized meal plans to achieve your dream!
+        </Text>
+
+        <TouchableOpacity style={styles.tombolawal}>
+          <Text>Register</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.tombolawal, {backgroundColor: 'lightblue'}]}>
+          <Text>Have Account</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fcfcfc',
+  },
+
+  inputBox: {
+    width: 280,
+    height: 35,
+    backgroundColor: 'tomato',
+    borderRadius: 25,
+    paddingHorizontal: 15,
+    fontSize: 15,
+    color: '#ffffff',
+    marginVertical: 12,
+  },
+
+  tombolawal: {
+    backgroundColor: 'pink',
+    width: '50%',
+    paddingVertical: 10,
+    marginVertical: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+});
